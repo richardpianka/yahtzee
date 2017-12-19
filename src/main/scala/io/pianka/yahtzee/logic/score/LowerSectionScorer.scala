3f0477ad370c4e5c22ef86c49e907e59065f84b0
Richard Pianka <richardpianka@gmail.com>
2017-12-18 23:27:13 -0500

    Added remaining scoring and tests for lower section.

--- src/main/scala/io/pianka/yahtzee/logic/score/LowerSectionScorer.scala
+++ src/main/scala/io/pianka/yahtzee/logic/score/LowerSectionScorer.scala
@@ -14,4 +14,34 @@ object LowerSectionScorer {
     require(LowerSectionChecker.fourOfAKind(roll))
     CommonScorer.sum(roll)
   }
+
+  private val fullHouseScore = 25
+  def scoreFullHouse(roll: Roll): Int = {
+    require(LowerSectionChecker.fullHouse(roll))
+    fullHouseScore
+  }
+
+  private val smallStraightScore = 30
+  def scoreSmallStraight(roll: Roll): Int = {
+    require(LowerSectionChecker.smallStraight(roll))
+    smallStraightScore
+  }
+
+  private val largeStraightScore = 40
+  def scoreLargeStraight(roll: Roll): Int = {
+    require(LowerSectionChecker.largeStraight(roll))
+    largeStraightScore
+  }
+
+  private val yahtzeeScore = 50
+  private val yahtzeeBonus = 100
+  def scoreYahtzee(roll: Roll, first: Boolean = true): Int = {
+    require(LowerSectionChecker.yahtzee(roll))
+
+    if (first) yahtzeeScore else yahtzeeBonus
+  }
+
+  def scoreChance(roll: Roll): Int = {
+    CommonScorer.sum(roll)
+  }
 }
--- src/test/scala/io/pianka/yahtzee/logic/score/LowerSectionScorerTest.scala
+++ src/test/scala/io/pianka/yahtzee/logic/score/LowerSectionScorerTest.scala
@@ -44,4 +44,67 @@ class LowerSectionScorerTest extends FlatSpec with Matchers {
       LowerSectionScorer.scoreThreeOfAKind(roll) should be (15)
     }
   }
+
+  /* Full house */
+  // Positive
+  "A three full of fours" should "score to twenty five" in {
+    val roll = createRoll(4, 3, 3, 4, 3)
+    LowerSectionScorer.scoreFullHouse(roll) should be (25)
+  }
+
+  "A two full of aces" should "score to twenty five" in {
+    val roll = createRoll(1, 2, 2, 2, 1)
+    LowerSectionScorer.scoreFullHouse(roll) should be (25)
+  }
+
+  /* Small straight */
+  // Positive
+  "One through four" should "score to thirty" in {
+    val roll = createRoll(1, 2, 3, 4, 4)
+    LowerSectionScorer.scoreSmallStraight(roll) should be (30)
+  }
+
+  "One through five" should "score to thirty" in {
+    val roll = createRoll(1, 2, 3, 4, 5)
+    LowerSectionScorer.scoreSmallStraight(roll) should be (30)
+  }
+
+  "Two through five" should "score to thirty" in {
+    val roll = createRoll(2, 3, 4, 5, 5)
+    LowerSectionScorer.scoreSmallStraight(roll) should be (30)
+  }
+
+  "Two through six" should "score to thirty" in {
+    val roll = createRoll(2, 3, 4, 5, 6)
+    LowerSectionScorer.scoreSmallStraight(roll) should be (30)
+  }
+
+  "Three through six" should "score to thirty" in {
+    val roll = createRoll(3, 4, 5, 6, 6)
+    LowerSectionScorer.scoreSmallStraight(roll) should be (30)
+  }
+
+  /* Large straight */
+  // Positive
+  "One through five" should "score to forty" in {
+    val roll = createRoll(1, 2, 3, 4, 5)
+    LowerSectionScorer.scoreLargeStraight(roll) should be (40)
+  }
+
+  "Two through six" should "score to forty" in {
+    val roll = createRoll(2, 3, 4, 5, 6)
+    LowerSectionScorer.scoreLargeStraight(roll) should be (40)
+  }
+
+  /* Yahtzee */
+  // Positive
+  "First yahtzee" should "score to fifty" in {
+    val roll = createRoll(1, 1, 1, 1, 1)
+    LowerSectionScorer.scoreYahtzee(roll) should be (50)
+  }
+
+  "Subsequent yahtzees" should "score to one hundred" in {
+    val roll = createRoll(1, 1, 1, 1, 1)
+    LowerSectionScorer.scoreYahtzee(roll, first = false) should be (100)
+  }
 }
